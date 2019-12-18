050d4825e1753ebff55adcb6a09ffc040dae4f2f
Christoph Pickl <christoph.pickl@ibood.com>
2019-12-18 17:57:26 +0100

    simple ktor endpoint

--- myfirst/src/main/kotlin/idiotlin/App.kt
+++ myfirst/src/main/kotlin/idiotlin/App.kt
@@ -1,8 +1,24 @@
 package idiotlin
 
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
 object App {
     @JvmStatic
     fun main(args: Array<String>) {
-        println("hello app")
+        val server = embeddedServer(Netty, port = 8080) {
+            routing {
+                get("/") {
+                    call.respondText("Hello Idiotlin!", ContentType.Text.Plain)
+                }
+            }
+        }
+        println("starting server ...")
+        server.start(wait = true)
     }
 }
