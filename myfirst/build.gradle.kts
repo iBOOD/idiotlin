65008527431a949a515427d8705b2591ead0f476
Christoph Pickl <christoph.pickl@gmail.com>
2020-01-28 11:06:27 +0100

    add buildSrc directory

--- myfirst/build.gradle.kts
+++ myfirst/build.gradle.kts
@@ -6,17 +6,6 @@ repositories {
     jcenter()
 }
 
-object Versions {
-    const val assertk = "0.20"
-    const val exposed = "0.17.7"
-    const val jsonAssert = "1.5.0"
-    const val klogging = "1.7.8"
-    const val kodein = "6.5.1"
-    const val ktor = "1.2.6"
-    const val logback = "1.2.3"
-    const val spek = "2.0.9"
-}
-
 plugins {
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.serialization") version "1.3.61"
