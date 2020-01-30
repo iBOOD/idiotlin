97167ba5e741387d5cf5c7eac84fbdab8246bf9f
Christoph Pickl <christoph.pickl@gmail.com>
2020-01-30 14:37:02 +0100

    really final preparations

--- execution/src/test/kotlin/idiotlin/testInfrastructure.kt
+++ execution/src/test/kotlin/idiotlin/testInfrastructure.kt
@@ -6,8 +6,8 @@ import org.json.JSONArray
 import org.skyscreamer.jsonassert.JSONAssert
 import org.skyscreamer.jsonassert.JSONCompareMode
 
-fun Assert<TestApplicationResponse>.contentEqualsArrayJson(expectedJson: String) {
+fun Assert<String?>.isEqualJsonArray(expectedJson: String) {
     given {
-        JSONAssert.assertEquals(expectedJson, JSONArray(it.content), JSONCompareMode.NON_EXTENSIBLE)
+        JSONAssert.assertEquals(expectedJson, JSONArray(it), JSONCompareMode.NON_EXTENSIBLE)
     }
 }
