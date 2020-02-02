fd3552edb8b561b5ce06d18b84869fe745f9a021
Christoph Pickl <christoph.pickl@gmail.com>
2020-02-02 11:36:11 +0100

    this is the real source code of the session i gave at FOSDEM 2020

--- execution/src/main/kotlin/idiotlin/App.kt
+++ execution/src/main/kotlin/idiotlin/App.kt
@@ -1,1 +1,87 @@
 package idiotlin
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import io.ktor.serialization.serialization
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
+
+object App {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        connectToDatabase()
+        embeddedServer(factory = Netty) {
+            ktor()
+        }.start(wait = true)
+    }
+}
+
+fun Application.ktor(kodein: Kodein = kodein()) {
+    val service by kodein.instance<Service>()
+    install(ContentNegotiation) {
+        serialization()
+    }
+    routing {
+        route("") {
+            get {
+                call.respond(service.readAll())
+            }
+        }
+    }
+}
+
+@Serializable
+data class Model(val name: String)
+
+interface Service {
+    fun readAll(): List<Model>
+}
+
+class DummyService(private val repo: ModelRepository) : Service {
+    override fun readAll() = repo.readAll()
+}
+
+fun kodein() = Kodein {
+    bind<Service>() with singleton { DummyService(instance()) }
+    bind<ModelRepository>() with singleton { ExposedModelRepository() }
+}
+
+interface ModelRepository {
+    fun readAll(): List<Model>
+}
+
+class ExposedModelRepository : ModelRepository {
+    override fun readAll() = transaction {
+        ModelTable.selectAll().map {
+            Model(name = it[ModelTable.name])
+        }
+    }
+}
+
+object ModelTable : Table() {
+    val name = varchar("name", length = 50)
+}
+
+fun connectToDatabase(url: String = "jdbc:h2:mem:idiotlinDb;DB_CLOSE_DELAY=-1") =
+    Database.Companion.connect(url, "org.h2.Driver").apply {
+        transaction {
+            SchemaUtils.create(ModelTable)
+        }
+    }
--- execution/src/test/kotlin/idiotlin/Test.kt
+++ execution/src/test/kotlin/idiotlin/Test.kt
@@ -1,1 +1,59 @@
 package idiotlin
+
+import assertk.Assert
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import org.json.JSONArray
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.singleton
+import org.skyscreamer.jsonassert.JSONAssert
+import org.skyscreamer.jsonassert.JSONCompareMode
+import org.testng.annotations.Test
+
+@Test
+class Test {
+
+    private val someModel = Model.any()
+
+    fun `When get root path Then return 200 OK`() {
+        connectToDatabase("jdbc:h2:mem:idiotlinTestDb;DB_CLOSE_DELAY=-1")
+        withTestApplication({
+            ktor()
+        }) {
+            with(handleRequest(HttpMethod.Get, "")) {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
+            }
+        }
+    }
+
+    fun `Given a stubbed service When get root path Then return proper JSON`() {
+        withTestApplication({
+            ktor(Kodein {
+                extend(kodein())
+                bind<Service>(overrides = true) with singleton { TestableService(someModel) }
+            })
+        }) {
+            with(handleRequest(HttpMethod.Get, "")) {
+                assertThat(response.content).isEqualJsonArray("""[{"name":"${someModel.name}"}]""")
+            }
+        }
+    }
+}
+
+class TestableService(private val models: List<Model>) : Service {
+    constructor(vararg models: Model) : this(models.toList())
+    override fun readAll() = models
+}
+
+fun Model.Companion.any() = Model("test")
+
+fun Assert<String?>.isEqualJsonArray(expectedJson: String) {
+    given {
+        JSONAssert.assertEquals(expectedJson, JSONArray(it), JSONCompareMode.NON_EXTENSIBLE)
+    }
+}
